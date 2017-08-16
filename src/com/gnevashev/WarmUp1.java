7f051c6c6ddcedfd11cfd25fd76184fe11b20fef
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2017-08-16 23:10:13 +0500

    Another 3 tasks

--- src/com/gnevashev/WarmUp1.java
+++ src/com/gnevashev/WarmUp1.java
@@ -4,30 +4,54 @@ public class WarmUp1 {
 
     //The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
     // We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-    public static boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!vacation && weekday) return false;
-        return true;
+    boolean sleepIn(boolean weekday, boolean vacation) {
+        return vacation || !weekday;
     }
 
     //We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
     // We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
 
-    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        if (aSmile && bSmile || !aSmile && !bSmile)
-        {
-            return true;
-        }
-        else
-            return false;
-
+    static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return (aSmile && bSmile || !aSmile && !bSmile);
     }
 
     //Given two int values, return their sum.
     // Unless the two values are the same, then return double their sum.
-    public int sumDouble(int a, int b) {
+    int sumDouble(int a, int b) {
        if (a == b)
            return (a+b)*2;
        else
            return (a + b);
     }
+
+    //Given an int n, return the absolute difference between n and 21,
+    // except return double the absolute difference if n is over 21.
+
+    public int diff21(int n) {
+        //if n is over 21 return double the absolute difference
+        if (n > 21)
+            return 2*(n-21);
+        //else return the difference between n and 21
+        else
+            return 21-n;
+    }
+
+    //We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
+    // We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && (hour < 7 || hour > 20)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+    public boolean makes10(int a, int b) {
+        return (a+b == 10 || a==10 || b==10);
+    }
+
+
+
 }
--- tests/com/gnevashev/WarmUp1Test.java
+++ tests/com/gnevashev/WarmUp1Test.java
@@ -29,4 +29,32 @@ class WarmUp1Test {
         assertEquals(5, w.sumDouble(3, 2));
         assertEquals(8, w.sumDouble(2, 2));
     }
+
+    @Test
+    void diff21() {
+        assertEquals(2, w.diff21(19));
+        assertEquals(11, w.diff21(10));
+        assertEquals(21, w.diff21(0));
+        assertEquals(8, w.diff21(25));
+    }
+    @Test
+    void parrotTrouble() {
+        assertTrue(w.parrotTrouble(true, 6));
+        assertTrue(w.parrotTrouble(true, 23));
+
+        assertFalse(w.parrotTrouble(true, 7));
+        assertFalse(w.parrotTrouble(false, 6));
+
+    }
+
+    @Test
+    void makes10() {
+        assertTrue(w.makes10(10, 0));
+        assertTrue(w.makes10(15, 10));
+        assertTrue(w.makes10(6, 4));
+        assertFalse(w.makes10(4, 4));
+
+    }
+
+
 }
