1320eef2b4c731906cec761d70901a3dca544e75
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2017-08-19 11:42:08 +0500

    Another 3 tasks

--- src/com/gnevashev/WarmUp1.java
+++ src/com/gnevashev/WarmUp1.java
@@ -1,6 +1,7 @@
 package com.gnevashev;
 
 import static java.lang.Math.abs;
+import static java.lang.Math.max;
 
 public class WarmUp1 {
 
@@ -99,5 +100,110 @@ public class WarmUp1 {
         String substr = str.substring(0, Math.min(str.length(), 3));
         return substr + substr + substr;
     }
+
+    //Given a string, take the last char and return a new string with the last char added at the front and back,
+    //so "cat" yields "tcatt". The original string will be length 1 or more.
+    public String backAround(String str) {
+        if (str == "") return "";
+        String lastCharacter = str.substring(str.length()-1);
+        return lastCharacter + str + lastCharacter;
+    }
+
+    //Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+    //Use the % "mod" operator -- see Introduction to Mod
+    public boolean or35(int n) {
+        return (n%3 == 0 || n%5 == 0);
+    }
+
+
+    //Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back,
+    //so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+    public String front22(String str) {
+        String beginString;
+        if (str.length() < 2) {
+            beginString = str;
+        }
+        else {
+            beginString = str.substring(0, 2);
+        }
+        return beginString + str + beginString;
+    }
+
+    //Given a string, return true if the string starts with "hi" and false otherwise.
+    public boolean startHi(String str) {
+        return str.startsWith("hi");
+    }
+
+    //Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 > 100 && temp2 < 0) || (temp1 < 0 && temp2 > 100);
+    }
+
+    //Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
+    }
+
+    //We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+    //Given 3 int values, return true if 1 or more of them are teen.
+    public boolean hasTeen(int a, int b, int c) {
+        return ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19));
+    }
+
+
+    //We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+    //Given 2 int values, return true if one or the other is teen, but not both.
+    public boolean loneTeen(int a, int b) {
+        int aTeen = 0;
+        int bTeen = 0;
+        if (a >= 13 && a <= 19)
+            aTeen = 1;
+        if (b >= 13 && b <= 19)
+            bTeen = 1;
+        return (aTeen + bTeen == 1);
+    }
+
+    //Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted.
+    //Otherwise, return the string unchanged.
+    public String delDel(String str) {
+        if (str.startsWith("del", 1)){
+            return str.replaceFirst("del", "");
+        }
+        return str;
+    }
+
+    //Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+    public boolean mixStart(String str) {
+        return (str.startsWith("ix", 1));
+    }
+
+
+    //Given a string, return a string made of the first 2 chars (if present),
+    //however include first char only if it is 'o' and include the second only if it is 'z',
+    //so "ozymandias" yields "oz".
+    public String startOz(String str) {
+        String retString = "";
+        int len = str.length();
+
+        if (len >= 1 && str.charAt(0)=='o') {
+            retString = retString + str.charAt(0);
+        }
+
+        if (len >= 2 && str.charAt(1)=='z') {
+            retString = retString + str.charAt(1);
+        }
+
+        return retString;
+    }
+
+    //Given three int values, a b c, return the largest.
+    public int intMax(int a, int b, int c) {
+        int maxValue;
+        maxValue = Math.max(a, b);
+        maxValue = Math.max(maxValue, c);
+        return maxValue;
+
+    }
+
 
 }
--- tests/com/gnevashev/WarmUp1Test.java
+++ tests/com/gnevashev/WarmUp1Test.java
@@ -5,14 +5,108 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarmUp1Test {
-
     WarmUp1 w = new WarmUp1();
+
+    @Test
+    void intMax() {
+        assertEquals(3, w.intMax(1, 2, 3));
+        assertEquals(3, w.intMax(1, 3, 2));
+        assertEquals(3, w.intMax(3, 2, 1));
+
+    }
+    @Test
+    void startOz() {
+        assertEquals("oz", w.startOz("ozymandias"));
+        assertEquals("z", w.startOz("bzoo"));
+        assertEquals("o", w.startOz("oxx"));
+        assertEquals("", w.startOz(""));
+    }
+
+    @Test
+    void mixStart() {
+        assertTrue(w.mixStart("mix snacks"));
+        assertTrue(w.mixStart("pix snacks"));
+        assertFalse(w.mixStart("piz snacks"));
+        assertFalse(w.mixStart(""));
+        assertFalse(w.mixStart("mi"));
+    }
+
+    @Test
+    void delDel() {
+        assertEquals("abc", w.delDel("adelbc"));
+        assertEquals("aHello", w.delDel("adelHello"));
+        assertEquals("adedbc", w.delDel("adedbc"));
+        assertEquals("a", w.delDel("a"));
+        assertEquals("", w.delDel(""));
+        assertEquals("adel", w.delDel("adeldel"));
+    }
+
+    @Test
+    void loneTeen() {
+        assertTrue(w.loneTeen(13, 99));
+        assertTrue(w.loneTeen(21, 19));
+        assertFalse(w.loneTeen(13, 13));
+    }
+    @Test
+    void hasTeen() {
+        assertTrue(w.hasTeen(13, 20, 10));
+        assertTrue(w.hasTeen(20, 19, 10));
+        assertTrue(w.hasTeen(20, 10, 13));
+        assertFalse(w.hasTeen(5, 25, 21));
+    }
+
+    @Test
+    void in1020() {
+        assertTrue(w.in1020(10, 99));
+        assertTrue(w.in1020(12, 99));
+        assertTrue(w.in1020(21, 20));
+        assertTrue(w.in1020(21, 15));
+        assertFalse(w.in1020(8, 99));
+    }
+
+    @Test
+    void icyHot() {
+        assertTrue(w.icyHot(120, -1));
+        assertTrue(w.icyHot(-1, 120));
+        assertFalse(w.icyHot(2, 120));
+        assertFalse(w.icyHot(-2, -120));
+        assertFalse(w.icyHot(-1, 100));
+    }
+
+    @Test
+    void startHi() {
+        assertTrue(w.startHi("hi there"));
+        assertTrue(w.startHi("hi"));
+        assertFalse(w.startHi("hello hi"));
+        assertFalse(w.startHi(""));
+    }
+
+    @Test
+    void front22() {
+        assertEquals("kikittenki", w.front22("kitten"));
+        assertEquals("HaHaHa", w.front22("Ha"));
+        assertEquals("ababcab", w.front22("abc"));
+        assertEquals("aaa", w.front22("a"));
+    }
+
+
+    @Test
+    void or35() {
+        assertTrue(w.or35(3));
+        assertTrue(w.or35(10));
+        assertFalse(w.or35(8));
+    }
+
+    @Test
+    void backAround() {
+        assertEquals(true, w.monkeyTrouble(true, true));
+    }
+
     @Test
     void monkeyTrouble() {
-        assertEquals(true, w.monkeyTrouble(true, true));
-        assertEquals(true, w.monkeyTrouble(false, false));
-        assertEquals(false, w.monkeyTrouble(true, false));
-        assertEquals(false, w.monkeyTrouble(false, true));
+        assertEquals("tcatt", w.backAround("cat"));
+        assertEquals("oHelloo", w.backAround("Hello"));
+        assertEquals("aaa", w.backAround("a"));
     }
 
     @Test
