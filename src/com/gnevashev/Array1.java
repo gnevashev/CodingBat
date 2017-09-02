5bea8da2320d676b3df03639c4b4bdaed9e08f7e
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2017-09-02 12:34:33 +0500

    fron11 method refactoring

--- src/com/gnevashev/Array1.java
+++ src/com/gnevashev/Array1.java
@@ -62,27 +62,16 @@ public class Array1 {
     //Given 2 int arrays, a and b, of any length, return a new array with the first element of each array.
     // If either array is length 0, ignore that array.
     public int[] front11(int[] a, int[] b) {
-        int[] retArray;
-
         if (a.length > 0 && b.length > 0) {
-            retArray = new int[2];
-            retArray[0] = a[0];
-            retArray[1] = b[0];
-            return retArray;
+            return new int[]{a[0], b[0]};
         }
-        if (a.length > 0 || b.length > 0) {
-            retArray = new int[1];
-            if (a.length > 0) {
-                retArray[0] = a[0];
-                return retArray;
-            }
-            if (b.length > 0 ) {
-                retArray[0] = b[0];
-                return retArray;
-            }
+        if (a.length > 0) {
+            return new int[]{a[0]};
         }
-        retArray = new int [0];
-        return retArray;
+        if (b.length > 0) {
+            return new int[]{b[0]};
+        }
+        return new int[]{};
     }
 
     //Given an array of ints length 3, figure out which is larger, the first or last element in the array,
