4b39388ffc5d6177764823d547dacc624d941f48
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2017-10-15 22:57:30 +0500

    Array 3 tasks

--- src/com/gnevashev/Array3.java
+++ src/com/gnevashev/Array3.java
@@ -37,5 +37,61 @@ public class Array3 {
         return nums;
     }
 
+    //(This is a slightly harder version of the fix34 problem.) Return an array that contains
+    //exactly the same numbers as the given array, but rearranged so that every 4 is immediately
+    //followed by a 5. Do not move the 4's, but every other number may move. The array contains
+    //the same number of 4's and 5's, and every 4 has a number after it that is not a 4.
+    //In this version, 5's may appear anywhere in the original array.
+    public int[] fix45(int[] nums) {
+        for (int i = 0, startJ = 0; i < nums.length; i++) {
+            if (nums[i] == 4 && nums[i+1] != 5) {
+                for (int j = startJ; j < nums.length; j++) {
+                    if (nums[j] == 5 && (j ==0 || nums[j - 1] != 4)) {
+                        nums[j] = nums[i+1];
+                        nums[i+1] = 5;
+                        startJ = j+1;
+                        break;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+
+    //Given a non-empty array, return true if there is a place to split the array so that the sum
+    //of the numbers on one side is equal to the sum of the numbers on the other side.
+    public boolean canBalance(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            leftSum += nums[i];
+            rightSum = 0;
+            for (int j = i+1; j < nums.length; j++) {
+                rightSum += nums[j];
+            }
+            if (leftSum == rightSum) return true;
+        }
+        return false;
+    }
+
+    //Given two arrays of ints sorted in increasing order, outer and inner, return true if all of
+    //the numbers in inner appear in outer. The best solution makes only a single "linear" pass of
+    //both arrays, taking advantage of the fact that both arrays are already in sorted order.
+    public boolean linearIn(int[] outer, int[] inner) {
+        boolean indicator = false;
+        for (int i = 0, startJ = 0; i < inner.length; i++) {
+            indicator = false;
+            for (int j = startJ; j < outer.length; j++) {
+                if (inner[i] == outer[j]) {
+                    indicator = true;
+                    startJ = j + 1;
+                    break;
+                }
+            }
+            if (!indicator) return false;
+        }
+        return true;
+    }
+
 
 }
--- tests/com/gnevashev/Array3Test.java
+++ tests/com/gnevashev/Array3Test.java
@@ -8,6 +8,43 @@ class Array3Test {
 
     Array3 a = new Array3();
 
+    //Given two arrays of ints sorted in increasing order, outer and inner, return true if all of
+    //the numbers in inner appear in outer. The best solution makes only a single "linear" pass of
+    //both arrays, taking advantage of the fact that both arrays are already in sorted order.
+    @Test
+    void linearIn() {
+        assertTrue(a.linearIn(new int[]{1, 2, 4, 6}, new int[]{2, 4}));
+        assertFalse(a.linearIn(new int[]{1, 2, 4, 6}, new int[]{2, 3, 4}));
+        assertTrue(a.linearIn(new int[]{1, 2, 4, 4, 6}, new int[]{2, 4}));
+        assertTrue(a.linearIn(new int[]{-1, 0, 3, 3, 3, 10, 12}, new int[]{-1, 0, 3, 12}));
+    }
+
+    //Given a non-empty array, return true if there is a place to split the array so that the sum
+    //of the numbers on one side is equal to the sum of the numbers on the other side.
+    @Test
+    void canBalance() {
+        assertTrue(a.canBalance(new int[]{1, 1, 1, 2, 1}));
+        assertFalse(a.canBalance(new int[]{2, 1, 1, 2, 1}));
+        assertFalse(a.canBalance(new int[]{2}));
+        assertTrue(a.canBalance(new int[]{10, 10}));
+    }
+
+    //(This is a slightly harder version of the fix34 problem.) Return an array that contains
+    //exactly the same numbers as the given array, but rearranged so that every 4 is immediately
+    //followed by a 5. Do not move the 4's, but every other number may move. The array contains
+    //the same number of 4's and 5's, and every 4 has a number after it that is not a 4.
+    //In this version, 5's may appear anywhere in the original array.
+    @Test
+    void fix45() {
+        assertArrayEquals(new int[]{9, 4, 5, 4, 5, 9}, a.fix45(new int[]{5, 4, 9, 4, 9, 5}));
+        assertArrayEquals(new int[]{1, 4, 5, 1}, a.fix45(new int[]{1, 4, 1, 5}));
+        assertArrayEquals(new int[]{1, 4, 5, 1, 1, 4, 5}, a.fix45(new int[]{1, 4, 1, 5, 5, 4, 1}));
+        assertArrayEquals(new int[]{1, 4, 5, 4, 5}, a.fix45(new int[]{5, 4, 5, 4, 1}));
+        assertArrayEquals(new int[]{1, 4, 5, 4, 5}, a.fix45(new int[]{5, 4, 5, 4, 1}));
+        assertArrayEquals(new int[]{4, 5, 4, 5, 1}, a.fix45(new int[]{4, 5, 4, 1, 5}));
+    }
+
+
     //Return an array that contains exactly the same numbers as the given array, but rearranged
     //so that every 3 is immediately followed by a 4. Do not move the 3's, but every other number may move.
     //The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3,
