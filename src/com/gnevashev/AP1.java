8812c30b53343a48a811a5a73b4c65350cdf6fd9
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2017-10-18 22:15:50 +0500

    AP-1 tasks

--- src/com/gnevashev/AP1.java
+++ src/com/gnevashev/AP1.java
@@ -1,5 +1,9 @@
 package com.gnevashev;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AP1 {
 
     //Given an array of scores, return true if each score is equal or greater than the one before.
@@ -26,11 +30,80 @@ public class AP1 {
     public boolean scoresClump(int[] scores) {
         if (scores.length < 3) return false;
         for (int i = 0; i < scores.length-2; i++) {
-            if (scores[i] <= scores[i+1] && scores[i+1] <= scores[i+2] && scores[i+2] - scores[i] <= 2) {
+            if (scores[i+2] - scores[i] <= 2) {
                 return true;
             }
         }
         return false;
     }
 
+    //Given an array of scores, compute the int average of the first half and the second half, and
+    //return whichever is larger. We'll say that the second half begins at index length/2. The
+    //array length will be at least 2. To practice decomposition, write a separate helper method
+    //int average(int[] scores, int start, int end) { which computes the average of the
+    //elements between indexes start..end. Call your helper method twice to implement scoresAverage().
+    //Write your helper method after your scoresAverage() method in the JavaBat text area.
+    //Normally you would compute averages with doubles, but here we use ints so the expected results are exact.
+    public int scoresAverage(int[] scores) {
+        return Integer.max(average(scores, 0, scores.length/2), average(scores, scores.length/2, scores.length));
+    }
+
+    int average(int[] scores, int start, int end){
+        int avg = 0;
+        for (int i = start; i < scores.length && i < end; i++) {
+            avg += scores[i];
+        }
+        avg = avg / (end - start);
+        return avg;
+    }
+
+    //Given an array of strings, return the count of the number of strings with the given length.
+    public int wordsCount(String[] words, int len) {
+        int count = 0;
+        for (String word : words) {
+            if (word.length() == len) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Given an array of strings, return a new array containing the first N strings.
+    //N will be in the range 1..length.
+    public String[] wordsFront(String[] words, int n) {
+        return Arrays.copyOfRange(words, 0, n);
+    }
+
+    //Given an array of strings, return a new List (e.g. an ArrayList) where all the strings of the
+    //given length are omitted. See wordsWithout() below which is more difficult because it uses arrays.
+    public List wordsWithoutList(String[] words, int len) {
+        List<String> result = new ArrayList<String>();
+        for (String word : words) {
+            if (word.length() != len) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+
+    //Given a positive int n, return true if it contains a 1 digit.
+    //Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+    public boolean hasOne(int n) {
+        return Integer.toString(n).matches("(.*)1(.*)");
+    }
+
+    //We'll say that a positive int divides itself if every digit in the number divides into the
+    //number evenly. So for example 128 divides itself since 1, 2, and 8 all divide into 128 evenly.
+    //We'll say that 0 does not divide into anything evenly, so no number with a 0 digit divides itself.
+    //Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+    public boolean dividesSelf(int n) {
+        int k = n;
+        int mod = 0;
+        while (k > 0) {
+            mod = k % 10;
+            if (mod == 0 || n % mod != 0) return false;
+            k = k / 10;
+        }
+        return true;
+    }
 }
--- tests/com/gnevashev/AP1Test.java
+++ tests/com/gnevashev/AP1Test.java
@@ -2,10 +2,81 @@ package com.gnevashev;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AP1Test {
+
     AP1 a = new AP1();
+
+    //We'll say that a positive int divides itself if every digit in the number divides into the
+    //number evenly. So for example 128 divides itself since 1, 2, and 8 all divide into 128 evenly.
+    //We'll say that 0 does not divide into anything evenly, so no number with a 0 digit divides itself.
+    //Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+    @Test
+    void dividesSelf() {
+        assertTrue(a.dividesSelf(128));
+        assertTrue(a.dividesSelf(12));
+        assertFalse(a.dividesSelf(120));
+        assertFalse(a.dividesSelf(42));
+    }
+
+    //Given a positive int n, return true if it contains a 1 digit.
+    //Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+    @Test
+    void hasOne() {
+        assertTrue(a.hasOne(10));
+        assertFalse(a.hasOne(22));
+        assertFalse(a.hasOne(22));
+        assertFalse(a.hasOne(220));
+    }
+
+    //Given an array of strings, return a new List (e.g. an ArrayList) where all the strings of the
+    //given length are omitted. See wordsWithout() below which is more difficult because it uses arrays.
+    @Test
+    void wordsWithoutList() {
+        assertEquals(Arrays.asList("bb", "ccc"), a.wordsWithoutList(new String[]{"a", "bb", "b", "ccc"}, 1));
+        assertEquals(Arrays.asList("a", "bb", "b"), a.wordsWithoutList(new String[]{"a", "bb", "b", "ccc"}, 3));
+        assertEquals(Arrays.asList("a", "bb", "b", "ccc"), a.wordsWithoutList(new String[]{"a", "bb", "b", "ccc"}, 4));
+    }
+
+    //Given an array of strings, return a new array containing the first N strings.
+    //N will be in the range 1..length.
+    @Test
+    void wordsFront() {
+        assertArrayEquals(new String[]{"a"}, a.wordsFront(new String[]{"a", "b", "c", "d"}, 1));
+        assertArrayEquals(new String[]{"a", "b"}, a.wordsFront(new String[]{"a", "b", "c", "d"}, 2));
+        assertArrayEquals(new String[]{"a", "b", "c"}, a.wordsFront(new String[]{"a", "b", "c", "d"}, 3));
+    }
+
+    @Test
+    void average() {
+    }
+
+    //Given an array of strings, return the count of the number of strings with the given length.
+    @Test
+    void wordsCount() {
+        assertEquals(2, a.wordsCount(new String[]{"a", "bb", "b", "ccc"}, 1));
+        assertEquals(1, a.wordsCount(new String[]{"a", "bb", "b", "ccc"}, 3));
+        assertEquals(0, a.wordsCount(new String[]{"a", "bb", "b", "ccc"}, 4));
+    }
+
+    //Given an array of scores, compute the int average of the first half and the second half, and
+    //return whichever is larger. We'll say that the second half begins at index length/2. The
+    //array length will be at least 2. To practice decomposition, write a separate helper method
+    //int average(int[] scores, int start, int end) { which computes the average of the
+    //elements between indexes start..end. Call your helper method twice to implement scoresAverage().
+    //Write your helper method after your scoresAverage() method in the JavaBat text area.
+    //Normally you would compute averages with doubles, but here we use ints so the expected results are exact.
+    @Test
+    void scoresAverage() {
+        assertEquals(4, a.scoresAverage(new int[]{2, 2, 4, 4}));
+        assertEquals(4, a.scoresAverage(new int[]{4, 4, 4, 2, 2, 2}));
+        assertEquals(4, a.scoresAverage(new int[]{3, 4, 5, 1, 2, 3}));
+        assertEquals(6, a.scoresAverage(new int[]{5, 6}));
+        assertEquals(5, a.scoresAverage(new int[]{5, 4}));
+    }
 
     //Given an array of scores sorted in increasing order, return true if the array
     //contains 3 adjacent scores that differ from each other by at most 2,
