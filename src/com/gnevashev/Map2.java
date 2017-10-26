2213f8c227febc721233f5d2f8cd1048304b5325
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2017-10-26 22:29:44 +0500

    Map-2 fisnished

--- src/com/gnevashev/Map2.java
+++ src/com/gnevashev/Map2.java
@@ -45,4 +45,101 @@ public class Map2 {
         }
         return map;
     }
+
+
+    //Given an array of non-empty strings, return a Map<String, String> with a key for every
+    //different first character seen, with the value of all the strings starting with that character
+    //appended together in the order they appear in the array.
+    public Map<String, String> firstChar(String[] strings) {
+        Map <String, String> map = new HashMap<>();
+        for (String s : strings) {
+            map.put(s.substring(0, 1), map.getOrDefault(s.substring(0, 1), "").concat(s));
+        }
+        return map;
+    }
+
+    //Loop over the given array of strings to build a result string like this: when a string appears
+    //the 2nd, 4th, 6th, etc. time in the array, append the string to the result.
+    //Return the empty string if no string appears a 2nd time.
+    public String wordAppend(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        StringBuffer sb = new StringBuffer(20);
+        for (String s : strings) {
+            if (map.containsKey(s)) {
+                sb.append(s);
+                map.remove(s);
+            }
+            else {
+                map.put(s, 1);
+            }
+        }
+        return sb.toString();
+    }
+
+    //Given an array of strings, return a Map<String, Boolean> where each different string is
+    //a key and its value is true if that string appears 2 or more times in the array.
+    public Map<String, Boolean> wordMultiple(String[] strings) {
+        Map<String, Boolean> map = new HashMap<>();
+        for (String s : strings) {
+            if (!map.containsKey(s)) {
+                map.put(s, false);
+            }
+            else if (!map.get(s)) {
+                map.put(s, true);
+            }
+        }
+        return map;
+    }
+
+    //We'll say that 2 strings "match" if they are non-empty and their first chars are the same.
+    //Loop over and then return the given array of non-empty strings as follows: if a string
+    //matches an earlier string in the array, swap the 2 strings in the array. When a position
+    //in the array has been swapped, it no longer matches anything. Using a map, this can be solved
+    //making just one pass over the array. More difficult than it looks.
+    public String[] allSwap(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        String key, tmp;
+        for (int i = 0; i < strings.length; i++) {
+            key = strings[i].substring(0, 1);
+            if (map.containsKey(key)) {
+                //change array elements and delete key
+                int j = map.get(key);
+                tmp = strings[i];
+                strings[i] = strings[j];
+                strings[j] = tmp;
+                map.remove(key);
+            }
+            else {
+                map.put(key, i);
+            }
+        }
+        return strings;
+    }
+
+    //We'll say that 2 strings "match" if they are non-empty and their first chars are the same.
+    //Loop over and then return the given array of non-empty strings as follows: if a string
+    //matches an earlier string in the array, swap the 2 strings in the array. A particular first
+    //char can only cause 1 swap, so once a char has caused a swap, its later swaps are disabled.
+    //Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+    public String[] firstSwap(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        String key, tmp;
+        for (int i = 0; i < strings.length; i++) {
+            key = strings[i].substring(0, 1);
+            if (map.containsKey(key)) {
+                if (map.get(key) >= 0) { //if swap enabled
+                    //change array elements and disable swap
+                    int j = map.get(key);
+                    tmp = strings[i];
+                    strings[i] = strings[j];
+                    strings[j] = tmp;
+                    map.put(key, -1);
+                }
+            }
+            else {
+                map.put(key, i);
+            }
+        }
+        return strings;
+    }
 }
--- tests/com/gnevashev/Map2Test.java
+++ tests/com/gnevashev/Map2Test.java
@@ -13,6 +13,101 @@ class Map2Test {
     private HashMap<String, Integer> map = new HashMap<>();
     private HashMap<String, Integer> expected = new HashMap<>();
 
+    //We'll say that 2 strings "match" if they are non-empty and their first chars are the same.
+    //Loop over and then return the given array of non-empty strings as follows: if a string
+    //matches an earlier string in the array, swap the 2 strings in the array. A particular first
+    //char can only cause 1 swap, so once a char has caused a swap, its later swaps are disabled.
+    //Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+    @Test
+    void firstSwap() {
+        assertArrayEquals(new String[]{"ac", "ab"}, m.firstSwap(new String[]{"ab", "ac"}));
+        assertArrayEquals(new String[]{"ay", "by", "cy", "cx", "bx", "ax", "aaa", "azz"}, m.firstSwap(new String[]{"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"}));
+        assertArrayEquals(new String[]{"ay", "by", "ax", "bx", "ai", "aj", "bx", "by"}, m.firstSwap(new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"}));
+
+    }
+
+    //We'll say that 2 strings "match" if they are non-empty and their first chars are the same.
+    //Loop over and then return the given array of non-empty strings as follows: if a string
+    //matches an earlier string in the array, swap the 2 strings in the array. When a position
+    //in the array has been swapped, it no longer matches anything. Using a map, this can be solved
+    //making just one pass over the array. More difficult than it looks.
+    @Test
+    void allSwap() {
+        assertArrayEquals(new String[]{"ac", "ab"}, m.allSwap(new String[]{"ab", "ac"}));
+        assertArrayEquals(new String[]{"ay", "by", "cy", "cx", "bx", "ax", "azz", "aaa"}, m.allSwap(new String[]{"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"}));
+        assertArrayEquals(new String[]{"ay", "by", "ax", "bx", "aj", "ai", "by", "bx"}, m.allSwap(new String[]{"ax", "bx", "ay", "by", "ai", "aj", "bx", "by"}));
+    }
+
+    //Given an array of strings, return a Map<String, Boolean> where each different string is
+    //a key and its value is true if that string appears 2 or more times in the array.
+    @Test
+    void wordMultiple() {
+        Map<String, Boolean> expected = new HashMap<>();
+        //testcase1
+        //wordMultiple(["a", "b", "a", "c", "b"]) → {"a": true, "b": true, "c": false}
+        map.clear();
+        expected.clear();
+        expected.put("a", true);
+        expected.put("b", true);
+        expected.put("c", false);
+        assertEquals(expected, m.wordMultiple(new String[]{"a", "b", "a", "c", "b"}));
+
+        //testcase2
+        //wordMultiple(["c", "b", "a"]) → {"a": false, "b": false, "c": false}
+        expected.clear();
+        expected.put("a", false);
+        expected.put("b", false);
+        expected.put("c", false);
+        assertEquals(expected, m.wordMultiple(new String[]{"c", "b", "a"}));
+
+        //testcase3
+        //wordMultiple(["c", "c", "c", "c"]) → {"c": true}
+        expected.clear();
+        expected.put("c", true);
+        assertEquals(expected, m.wordMultiple(new String[]{"c", "c", "c", "c"}));
+
+    }
+
+    //Loop over the given array of strings to build a result string like this: when a string appears
+    //the 2nd, 4th, 6th, etc. time in the array, append the string to the result.
+    //Return the empty string if no string appears a 2nd time.
+    @Test
+    void wordAppend() {
+        assertEquals("a", m.wordAppend(new String[]{"a", "b", "a"}));
+        assertEquals("aa", m.wordAppend(new String[]{"a", "b", "a", "c", "a", "d", "a"}));
+        assertEquals("a", m.wordAppend(new String[]{"a", "", "a"}));
+    }
+
+    //Given an array of non-empty strings, return a Map<String, String> with a key for every
+    //different first character seen, with the value of all the strings starting with that character
+    //appended together in the order they appear in the array.
+    @Test
+    void firstChar() {
+        Map<String, String> expected = new HashMap<>();
+        //testcase1
+        //firstChar(["salt", "tea", "soda", "toast"]) → {"s": "saltsoda", "t": "teatoast"}
+        map.clear();
+        expected.clear();
+        expected.put("s", "saltsoda");
+        expected.put("t", "teatoast");
+
+        assertEquals(expected, m.firstChar(new String[]{"salt", "tea", "soda", "toast"}));
+
+        //testcase2
+        //firstChar(["aa", "bb", "cc", "aAA", "cCC", "d"]) → {"a": "aaaAA", "b": "bb", "c": "cccCC", "d": "d"}
+        expected.clear();
+        expected.put("a", "aaaAA");
+        expected.put("b", "bb");
+        expected.put("c", "cccCC");
+        expected.put("d", "d");
+        assertEquals(expected, m.firstChar(new String[]{"aa", "bb", "cc", "aAA", "cCC", "d"}));
+
+        //testcase3
+        //firstChar([]) → {}
+        expected.clear();
+        assertEquals(expected, m.firstChar(new String[]{}));
+    }
+
     //The classic word-count algorithm: given an array of strings, return a Map<String, Integer>
     //with a key for each different string, with the value the number of times that string appears in the array.
     @Test
